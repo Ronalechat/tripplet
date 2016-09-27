c1e41ad880dd4f9a71f6562d739f47f8a77a3818
Pinky <dir.michaellin@gmail.com>
2016-09-27 21:00:13 +1000

    Add README.md. Additional changes made based on feedback from Luke

---
+++ README.md
@@ -0,0 +1,27 @@
+# TTT
+##### AKA Tic-Tac-Toe
+
+## What is this?
+This is a game of Tic-Tac-Toe, made with HTML, CSS, and Javascript. If you would like to know more about the game, and its history, I suggest you visit
+[this Wikipedia article on Tic-Tac-Toe](https://en.wikipedia.org/wiki/Tic-tac-toe)
+
+## Rules
+TTT is a two player game, one as the 'X', and the other as the 'O'. By default, 'O' has the first move. Each player has one move per turn. Each player's objective is to occupy three squares in a row with your symbol. If either player fails to occupy three squares by the ninth turn, it is considered a tie. Or a draw. No one wins.
+
+Turns are made simply by clicking on any unoccupied square, marked by a "+".
+
+## Win Conditions
+Winning happens when one player's "X" or "O" occupy three squares in a single direction.
+This can happen in any direction.
+
+#### Horizontal victory:
+![Horizontal win](http://imgur.com/LVqPTCG.png)
+
+#### Vertical victory:
+![Vertical win](http://imgur.com/XIluN3y.png)
+
+#### Diagonal victory:
+![Diagonal win](http://imgur.com/jORrjzS.png)
+
+## Why?
+This is a game made as an exercise in javascript as an assignment for General Assembly by Michael Lin. If you stake your claim in owning tic-tac-toe, you're probably a liar. This game was made as a practice in programming, as well as for the fun of it.
--- js/main.js
+++ js/main.js
@@ -1,69 +1,73 @@
+
 $(document).ready(function() {
   var gameBoard = [
-  ['','',''],
-  ['','',''],
-  ['','','']
-];
-var x = "x";
-var o = "o";
+    ['','',''],
+    ['','',''],
+    ['','','']
+  ];
+  var x = "x";
+  var o = "o";
 
-var player = o;
-var turns = 0;
+  var player = o;
+  var turns = 0;
 
-var win = false;
+  var win = false;
 
-// Horizontal and Vertical win conditions //////////////////////////////////
-var straightWin = function(player) {
-  // Check if xPos match
-  for (i = 0; i < 3; i++){
-    if (gameBoard[i][0] === player && gameBoard[i][0] === gameBoard[i][1] && gameBoard[i][1] === gameBoard[i][2]){
-      $('#winbanner').fadeIn().text('Winner is ' + player);
-      $('#playagain').css('display', 'inline');
-      return win = true; // FIXME: Add proper win condition
-    } else if (gameBoard[0][i] === player && gameBoard[0][i] === gameBoard[1][i] && gameBoard[1][i] === gameBoard[2][i]){
+  // Horizontal and Vertical win conditions //////////////////////////////////
+  var straightWin = function(player) {
+    // Check if xPos match
+    for (i = 0; i < 3; i++){
+      if (gameBoard[i][0] === player && gameBoard[i][0] === gameBoard[i][1] && gameBoard[i][1] === gameBoard[i][2]){
+        $('#winbanner').fadeIn().text('Winner is ' + player);
+        $('#playagain').css('display', 'inline');
+        return win = true; // FIXME: Add proper win condition
+      } else if (gameBoard[0][i] === player && gameBoard[0][i] === gameBoard[1][i] && gameBoard[1][i] === gameBoard[2][i]){
+        $('#winbanner').fadeIn().text('Winner is ' + player);
+        $('#playagain').css('display', 'inline');
+        return win = true; // FIXME: same as above
+      }
+    }
+  };
+
+  // Diagonal Win conditions //////////////////////////////////////////////
+  var diagonalWin = function(player) {
+    if (
+      gameBoard[0][0] === player && gameBoard[0][0] === gameBoard[1][1] && gameBoard[1][1] === gameBoard[2][2] || gameBoard[0][2] === player && gameBoard[0][2] === gameBoard[1][1] && gameBoard[1][1] === gameBoard[2][0]
+      ) {
       $('#winbanner').fadeIn().text('Winner is ' + player);
       $('#playagain').css('display', 'inline');
       return win = true; // FIXME: same as above
-    }
-  }
-};
+      }
+    };
 
-// Diagonal Win conditions //////////////////////////////////////////////
-var diagonalWin = function(player) {
-  if (
-    gameBoard[0][0] === player && gameBoard[0][0] === gameBoard[1][1] && gameBoard[1][1] === gameBoard[2][2] || gameBoard[0][2] === player && gameBoard[0][2] === gameBoard[1][1] && gameBoard[1][1] === gameBoard[2][0]
-    ) {
-    $('#winbanner').fadeIn().text('Winner is ' + player);
-    $('#playagain').css('display', 'inline');
-    return win = true; // FIXME: same as above
-    }
+  // Combined win function ////////////////////////////
+  var isWinner = function(player) {
+    straightWin(player);
+    diagonalWin(player);
   };
 
-// Combined win function ////////////////////////////
-var isWinner = function(player) {
-  straightWin(player);
-  diagonalWin(player);
-};
-
-// Parse options for player move //////////////////////////////////
-var playMove = function (posX, posY) {
-  isWinner(player);
-  if ( win === false && turns === 8 ){
-    $('#winbanner').fadeIn().text('Tie');
-    $('#playagain').css('display', 'inline');
-  };
-  if (player === o) {
-    player = x;
-  } else {
-    player = o;
-  }
-}; // End of PlayMove //////////////////
+  // Parse options for player move //////////////////////////////////
+  var playMove = function (posX, posY) {
+    isWinner(player);
+    if ( win === false && turns === 8 ){
+      $('#winbanner').fadeIn().text('Tie');
+      $('#playagain').css('display', 'inline');
+    };
+    if (player === o) {
+      player = x;
+    } else {
+      player = o;
+    }
+  }; // End of PlayMove //////////////////
 
   $('#board li').on('click', function(){
+    if (win === true) {
+      return; // don't do anything else in the click handler if the game has been won
+    }
     var posX = $(this).data('xpos');
     var posY = $(this).data('ypos');
     if (gameBoard[posX][posY] !== '') {
-      alert('Make another move.')
+      return;
     } else {
       $(this).text(player);
       gameBoard[posX][posY] = player;
@@ -76,6 +80,7 @@ var playMove = function (posX, posY) {
     $('#board li').text('+');
     turns = 0;
     win = false;
+    player = o;
     gameBoard = [
       ['','',''],
       ['','',''],
